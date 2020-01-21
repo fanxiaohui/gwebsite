82d1f68704e7c49780dc46eb30779167f84707d1
mo <thinkgo@aliyun.com>
2020-01-22 00:08:27 +0800

    完全显示采集任务节点

--- src/components/Master.vue
+++ src/components/Master.vue
@@ -4,20 +4,58 @@
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>{{tabPath()}}</el-breadcrumb-item>
     </el-breadcrumb>
-    <el-card>
+    <el-card shadow="hover">
       <div slot="header" class="clearfix">
         <strong>采集任务</strong>
       </div>
-      <el-table :data="nodes">
-        <el-table-column prop="portName" label="接口"/>
-        <el-table-column v-if="portName === 'Ethernet'" prop="ipAddress" label="从站IP"/>
-        <el-table-column prop="slaveID" label="从站地址"/>
-        <el-table-column prop="slaveID" label="功能码"/>
-        <el-table-column prop="coilAddress" label="数据地址"/>
-        <el-table-column prop="coilQuantity" label="数量"/>
-        <el-table-column prop="coilVirtualAddress" label="虚拟地址"/>
-        <el-table-column prop="coilScanRate" label="扫描周期"/>
-        <el-table-column label="配置">
+      <el-table :data="nodes" border class="node_table">
+        <el-table-column prop="portName" label="接口" width="80px" align="center"/>
+        <el-table-column v-if="portName === 'Ethernet'" prop="ipAddress" label="从站IP" width="200px" align="center"/>
+        <el-table-column prop="slaveID" label="从站地址" width="80px" align="center"/>
+        <el-table-column label="功能码参数" align="center">
+          <template slot-scope="props">
+            <table class="node_internal_table">
+              <thead>
+              <tr>
+                <th>功能码</th>
+                <th>数据地址</th>
+                <th>数量</th>
+                <th>虚拟地址</th>
+                <th>扫描周期(ms)</th>
+              </tr>
+              </thead>
+              <tr v-show="props.row.hasCoil">
+                <td>1</td>
+                <td>{{props.row.coilAddress}}</td>
+                <td>{{props.row.coilQuantity}}</td>
+                <td>{{props.row.coilVirtualAddress}}</td>
+                <td>{{props.row.coilScanRate}}</td>
+              </tr>
+              <tr v-show="props.row.hasDiscrete">
+                <td>2</td>
+                <td>{{props.row.discreteAddress}}</td>
+                <td>{{props.row.discreteQuantity}}</td>
+                <td>{{props.row.discreteVirtualAddress}}</td>
+                <td>{{props.row.discreteScanRate}}</td>
+              </tr>
+              <tr v-show="props.row.hasHolding">
+                <td>3</td>
+                <td>{{props.row.holdingAddress}}</td>
+                <td>{{props.row.holdingQuantity}}</td>
+                <td>{{props.row.holdingVirtualAddress}}</td>
+                <td>{{props.row.holdingScanRate}}</td>
+              </tr>
+              <tr v-show="props.row.hasInput">
+                <td>4</td>
+                <td>{{props.row.inputAddress}}</td>
+                <td>{{props.row.inputQuantity}}</td>
+                <td>{{props.row.inputVirtualAddress}}</td>
+                <td>{{props.row.inputScanRate}}</td>
+              </tr>
+            </table>
+          </template>
+        </el-table-column>
+        <el-table-column label="配置" width="200px" align="center">
           <template slot-scope="scope">
             <el-button type="primate" @click="editNode(scope.row.id)">编辑</el-button>
             <el-button type="danger" @click="deleteNode(scope.row.id)">删除</el-button>
@@ -25,7 +63,7 @@
         </el-table-column>
       </el-table>
       <div class="action_btns">
-        <el-button type="primary" round @click="showProtoDialog">编辑当前协议</el-button>
+        <el-button type="primary" round @click="showProtocolDialog">编辑当前协议</el-button>
         <el-button type="primary" round @click="showAddNodeDialog">添加从机</el-button>
         <el-button type="danger" round @click="deleteNode(0)">全部删除</el-button>
         <el-button type="info" round @click="hpVisible=true">帮助</el-button>
@@ -57,7 +95,7 @@
         </el-form-item>
       </el-form>
       <span slot="footer" class="form_btns">
-          <el-button type="primary" round @click="doConfirm">确定</el-button>
+          <el-button type="primary" round @click="editProtocol">确定</el-button>
           <el-button type="info" round @click="cancelPortoDialog">取消</el-button>
           <el-button type="info" round @click="hpVisible=true">帮助</el-button>
         </span>
@@ -262,7 +300,7 @@ export default {
     tabPath: function () {
       return this.portName === 'Any' ? '所有从站' : (this.portName === 'Ethernet' ? '以太网' : this.portName)
     },
-    showProtoDialog: function () {
+    showProtocolDialog: function () {
       if (this.portName !== 'Ethernet') {
         this.getPort()
       }
@@ -276,24 +314,6 @@ export default {
     },
     cancelAddNodeDialog: function () {
       this.addSlaveDialogVisible = false
-    },
-    doConfirm: async function () {
-      let url = '/gather/usart'
-      let inter = {
-        portName: this.protoFormData.portName,
-        feature: this.protoFormData.feature,
-        responseTimeout: this.protoFormData.responseTimeout,
-        delayPoll: this.protoFormData.delayPoll,
-        ipAddress: ''
-      }
-
-      if (this.portName === 'Ethernet') {
-        inter.ipAddress = this.protoFormData.ipAddress + ':' + this.protoFormData.ipPort
-        inter.portName = ''
-        url = '/gather/ethernet?action=add'
-      }
-      await this.$http.post(url, inter)
-      this.protoDialogVisible = false
     },
     getNodes: async function () {
       let url = '/gather/modbus/node' + ((this.portName !== 'Any') ? ('?portName=' + this.portName) : '')
@@ -337,6 +357,24 @@ export default {
       } catch (e) {
         console.log(e)
       }
+    },
+    editProtocol: async function () {
+      let url = '/gather/usart'
+      let inter = {
+        portName: this.protoFormData.portName,
+        feature: this.protoFormData.feature,
+        responseTimeout: this.protoFormData.responseTimeout,
+        delayPoll: this.protoFormData.delayPoll,
+        ipAddress: ''
+      }
+
+      if (this.portName === 'Ethernet') {
+        inter.ipAddress = this.protoFormData.ipAddress + ':' + this.protoFormData.ipPort
+        inter.portName = ''
+        url = '/gather/ethernet?action=add'
+      }
+      await this.$http.post(url, inter)
+      this.protoDialogVisible = false
     }
   },
   mounted() {
@@ -346,6 +384,22 @@ export default {
 </script>
 
 <style lang="less" scoped>
+  .node_table {
+    .el-form-item {
+      padding: 0;
+    }
+
+    .node_internal_table {
+      width: 100%;
+      border: 1px solid #aaa;
+    }
+
+    th, td {
+      padding: 0;
+      text-align: center;
+    }
+  }
+
   .action_btns {
     margin-top: 20px;
   }
