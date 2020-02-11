38c38e3814c29540aec9f3df4dc0d2ca958930f1
mo <thinkgo@aliyun.com>
2020-02-11 20:01:50 +0800

    修改query参数

--- src/components/Slave.vue
+++ src/components/Slave.vue
@@ -101,8 +101,8 @@ export default {
         bindPortName: 'Port1'
       },
       queryInfo: {
-        pageNum: 1,
-        pageSize: 10
+        pagenum: 1,
+        pagesize: 10
       },
       bindInterList: [],
       nodes: [],
@@ -131,11 +131,11 @@ export default {
       this.hpVisible = true
     },
     handleSizeChange(newSize) {
-      this.queryInfo.pageSize = newSize
+      this.queryInfo.pagesize = newSize
       this.getSlaves()
     },
     handleCurrentChange(newPage) {
-      this.queryInfo.pageNum = newPage
+      this.queryInfo.pagenum = newPage
       this.getSlaves()
     },
     getBindInterface: async function() {
